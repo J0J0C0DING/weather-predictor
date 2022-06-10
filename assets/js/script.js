1f78f0a2b65494dfaa7c1174bad0ef5b5fbdc43b
J0J0C0DING <102717784+J0J0C0DING@users.noreply.github.com>
2022-06-10 11:47:27 -0500

    Icons work properly, weather containers update after search or history is initated

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,15 +20,17 @@ $(searchBtn).on("click", function () {
   let citySearch = { city: searchInquery };
   // Place new search at beginning of savedCities array
   savedCities.unshift(citySearch);
-
   // Save to local storage
   localStorage.setItem("city", JSON.stringify(savedCities));
 
   historyDisplay();
+
+  searchInput.value = "";
 });
 
 // Search History
 const historyDisplay = function () {
+  // Limit to 5 search histories
   savedCities.splice(5);
   // Empty search history container
   $(searchHistory).empty();
@@ -42,7 +44,7 @@ historyDisplay();
 // Get location coordinates
 const getLocationCoord = function (city) {
   // Get OpenWeather API url
-  let apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=d1db20b74fefd2cbae86402d9eb44d4e`;
+  let apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=d1db20b74fefd2cbae86402d9eb44d4e`;
   // Make request to URL
   fetch(apiUrl)
     .then(function (response) {
@@ -73,18 +75,34 @@ const getWeather = function (lat, lon, name) {
       if (response.ok) {
         response.json().then(function (data) {
           console.log(data);
+          // Get current weather data
           let currentWeather = data.current;
+          // Get current weather icon code
+          let icon = currentWeather.weather[0].icon;
+          // Get city name from parent function
           let cityName = name;
-          displayWeather(cityName, 0, currentWeather.temp, currentWeather.wind_speed, currentWeather.humidity, currentWeather.uvi);
+          // Pass current weather items to display weather function for display
+          displayWeather(cityName, 0, currentWeather.temp, currentWeather.wind_speed, currentWeather.humidity, currentWeather.uvi, icon);
 
-          let forecaseWeather = data.daily.slice(0, 6);
+          // Only grab the the next 5 days
+          let forecastedWeather = data.daily.slice(0, 6);
+          console.log(forecastedWeather);
           // Loop through current + 5 days
-          for (i = 1; i < forecaseWeather.length; i++) {
+          for (i = 1; i < forecastedWeather.length; i++) {
+            // Get day id (starting at index 1)
             let day = i;
-            let temp = forecaseWeather[i].temp.day;
-            let wind = forecaseWeather[i].wind_speed;
-            let humidity = forecaseWeather[i].humidity;
-            displayWeather(cityName, day, temp, wind, humidity);
+            // Get temp
+            let temp = forecastedWeather[i].temp.day;
+            // Get wind speed
+            let wind = forecastedWeather[i].wind_speed;
+            // Get humidity
+            let humidity = forecastedWeather[i].humidity;
+            // Get forecasted weather icon
+            let icon = forecastedWeather[i].weather[0].icon;
+            // Pass empty uvi value
+            let uvi = "";
+            // Pass each value to display weather
+            displayWeather(cityName, day, temp, wind, humidity, uvi, icon);
           }
         });
       } else {
@@ -96,7 +114,7 @@ const getWeather = function (lat, lon, name) {
     });
 };
 
-const displayWeather = function (city, day, temp, wind, humidity, uvi) {
+const displayWeather = function (city, day, temp, wind, humidity, uvi, icon) {
   // Update City Name
   $(cityNameSpan).text(city);
 
@@ -128,32 +146,50 @@ const displayWeather = function (city, day, temp, wind, humidity, uvi) {
   uviLi.classList.add("weather-item");
 
   // Create span element to show severity of UV index
-  let uviSpan = document.createElement("span");
+  let uviSpan = document.createElement("div");
+  uviSpan.classList.add("uvi-div");
   uviSpan.textContent = uvi;
 
   // Depending on uvi, change span background color
   if (uvi < 3) {
     uviSpan.classList.add("uv-low");
-  } else if (3 <= uvi <= 5) {
+  } else if (3 < uvi && uvi < 6) {
     uviSpan.classList.add("uv-mod");
-  } else if (6 <= uvi <= 7) {
+  } else if (6 < uvi && uvi < 8) {
     uviSpan.classList.add("uv-high");
-  } else if (8 <= uvi <= 10) {
+  } else if (8 < uvi && uvi < 10) {
     uviSpan.classList.add("uv-very-high");
   } else {
     uviSpan.classList.add("uv-extreme");
   }
   uviLi.append(uviSpan);
 
+  // Get icon for display
+  let weatherIconUrl = `http://openweathermap.org/img/wn/${icon}.png`;
+  let iconEl = document.createElement("img");
+  iconEl.setAttribute("src", weatherIconUrl);
+  iconEl.classList.add("weather-item");
   // For day 0 add everything
   if (day === 0) {
-    $(weatherListEl).append(tempLi, windLi, humidityLi, uviLi);
+    $(weatherListEl).append(iconEl, tempLi, windLi, humidityLi, uviLi);
     $(mainWeatherEl).append(weatherListEl);
     $(choosenBlock).append(mainWeatherEl);
     // For every other day, do not add uv index
   } else {
-    $(weatherListEl).append(tempLi, windLi, humidityLi);
+    $(weatherListEl).append(iconEl, tempLi, windLi, humidityLi);
     $(mainWeatherEl).append(weatherListEl);
     $(choosenBlock).append(mainWeatherEl);
   }
 };
+
+// On load, display first city in local storage
+$(document).ready(function () {
+  if (savedCities[0]) {
+    getLocationCoord(savedCities[0].city);
+  }
+});
+
+$(".historyItem").click(function (event) {
+  getLocationCoord(event.target.innerHTML);
+  $("").replaceAll(".weather-item");
+});
